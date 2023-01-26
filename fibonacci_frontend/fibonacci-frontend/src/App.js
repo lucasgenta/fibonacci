8b9f2fc6f2b10d5d8a04dd5bfe2d2c6ebb7daea9
Lucas <luquigenta@gmail.com>
2023-01-26 15:43:35 +0000

    edited App.js

--- fibonacci_frontend/fibonacci-frontend/src/App.js
+++ fibonacci_frontend/fibonacci-frontend/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function FibonacciCalculator() {
+  const [inputValue, setInputValue] = useState('');
+  const [result, setResult] = useState('');
+
+  function handleInputChange(event) {
+    setInputValue(event.target.value);
+  }
+
+  async function calculateFibonacci() {
+    const response = await fetch(`/fibonacci?n=${inputValue}`);
+    const result = await response.text();
+    setResult(result);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Fibonacci Calculator</h1>
+      <form>
+        <label htmlFor="input-n">Enter a number:</label>
+        <input type="number" id="input-n" name="n" min="0" value={inputValue} onChange={handleInputChange} />
+        <button type="button" onClick={calculateFibonacci}>Calculate</button>
+      </form>
+      <p>{result ? `Result: ${result}` : ''}</p>
     </div>
   );
 }
 
-export default App;
+export default FibonacciCalculator;
