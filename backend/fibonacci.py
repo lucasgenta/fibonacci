255045df694b257d0cdb84b3615484bd1f59cc7e
Lucas <luquigenta@gmail.com>
2023-05-08 17:30:28 +0000

    added postgres, port binding was conflicting with telegram bot

--- backend/fibonacci.py
+++ backend/fibonacci.py
@@ -1,27 +1,31 @@
-import time
-from pymongo import MongoClient
+
+import psycopg2
 from flask import Flask, request, make_response
 from flask_cors import CORS
-import os
-from loguru import logger
-
 
 app = Flask(__name__)
 CORS(app)
 
-app = Flask(__name__)
-app.debug = True  # Enable debug mode
+conn = psycopg2.connect(
+    host="database",
+    database="fibonacci",
+    user="postgres",
+    password="your_password",
+    port=5432
+)
 
-# Set up logger
-#logger.add("app.log", rotation="500 MB", level="INFO")
+cur = conn.cursor()
 
-
-# Set up database connection
-client = MongoClient("mongodb://root:pass@:27017/")
-db = client["fibonacci_db"]
-collection = db["logs"]
-handler = logger.handlers.MongoDBHandler(collection)
-logger.add(handler)
+# Create the results table if it doesn't exist
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS results (
+        id SERIAL PRIMARY KEY,
+        ip TEXT NOT NULL,
+        n INTEGER NOT NULL,
+        result INTEGER NOT NULL
+    );
+""")
+conn.commit()
 
 def fibonacci(n):
     if n <= 0:
@@ -31,33 +35,16 @@ def fibonacci(n):
     else:
         return fibonacci(n-1) + fibonacci(n-2)
 
-
 @app.route('/fibonacci', methods=['GET'])
 def get_fibonacci():
-    # Log IP address and request
-    ip_address = request.remote_addr
-    http_method = request.method
-    url = request.url
-    logger.info(f'{ip_address} - {http_method} {url}')
-
-    # Calculate how long the request takes
-    start_time = time.time()
-
-    n = int(request.args.get('n'))
-
-    # Store result in database
-    response = make_response(str(fibonacci(n)))
-
-    response.headers.set('Access-Control-Allow-Origin', '*')
-
-    collection.insert_one({"n": n, "result": response})
-    # Log how long the request took
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    logger.info(f'Time taken: {elapsed_time}')
-
+    x = int(request.args.get('n'))
+    ip = request.remote_addr
+    result = fibonacci(x)
+    cur.execute("INSERT INTO results (ip, n, result) VALUES (%s, %s, %s)", (ip, x, result))
+    conn.commit()
+    response = make_response(str(result))
+    response.headers['Access-Control-Allow-Origin'] = '*'
     return response
 
-
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- backend/start_me_up.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Starting React app"
-
-pm2 start 0 
-
-echo "starting Docker container"
-
-docker compose up -d
